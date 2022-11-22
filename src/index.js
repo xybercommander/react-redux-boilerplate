ab9333793188242b18f529690d99e8858f34c181
Samrat <samratmukherjee.sps@gmail.com>
2022-11-22 13:50:52 +0530

    init commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
+
+// // STORE -> Globalized State
+
+
+// // ACTION -> INCREMENT
+// const increment  = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
+
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   }
+// }
+
+// // REDUCER -> Checks which Action is getting called
+// const counter = (state = 0, action) => {
+//   switch(action.type) {
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;
+//   }
+// }
+
+// let store = createStore(counter);
+// store.subscribe(() => console.log(store.getState()));
+
+// // DISPATCH -> Executes the action
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
+
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
